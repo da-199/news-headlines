6d60aa79f6fbe5534d7c65a8c8784ecd5f25ef76
dhruvi-9 <100179105+dhruvi-9@users.noreply.github.com>
2023-01-16 00:13:22 +0530

    Create nyt.py

---
+++ sources/nyt.py
@@ -0,0 +1,39 @@
+import requests
+import boto3
+from botocore.exceptions import ClientError
+import json
+
+def nyt(event, context):
+
+    secret_name = "nyt_api_access"
+    region_name = "us-east-1"
+
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        raise e
+
+    secret = get_secret_value_response['SecretString']
+    secret = json.loads(secret)
+    key = secret['key']
+    
+    url = f'https://api.nytimes.com/svc/topstories/v2/home.json?api-key={key}'
+    
+    response = requests.get(url).json()
+    
+    data = []
+    
+    for i in response['results']:
+        data.append(i['title'])
+    
+    destination = 'NYT'
+    
+    return {'data': data, 'destination': destination}
