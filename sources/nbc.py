f2430a6c8be6287e40e3b22284f1d9ba2f287ff6
dhruvi-9 <100179105+dhruvi-9@users.noreply.github.com>
2023-02-23 00:35:10 -0500

    Update nbc.py

--- sources/nbc.py
+++ sources/nbc.py
@@ -1,28 +1,28 @@
 import time
 import re
-from bs4 import BeautifulSoup
 import requests
+from bs4 import BeautifulSoup
 
 def nbc(event, context):
     
     url = 'https://www.nbcnews.com/'
-	response = requests.get(url)
-	soup = BeautifulSoup(response.text, 'html.parser')
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
 
-	class_dict = {'tease-card__headline':'h2',
-	'styles_headline__5qvTg':'h3',
-	'styles_headline__ice3t':'h2',
-	'styles_baconCardsWidth':'h3',
-	'related-content__headline-link':'a',
-	'cover-spread':'h2'}
+    class_dict = {'tease-card__headline':'h2',
+    'styles_headline__5qvTg':'h3',
+    'styles_headline__ice3t':'h2',
+    'styles_baconCardsWidth':'h3',
+    'related-content__headline-link':'a',
+    'cover-spread':'h2'}
     
-    data = parse(class_dict)
+    data = parse(soup, class_dict)
     
     destination = 'NBC'
     
     return {'data': data, 'destination': destination}
     
-def parse(class_dict):
+def parse(soup, class_dict):
     final = []    
     
     for key, value in class_dict.items():
@@ -30,4 +30,3 @@ def parse(class_dict):
         final.extend(lst)
         
     return final
-
