7ef303589612a15f2952827d8077bc820fc3df39
Dhruvi Antala <dhruviantala@Dhruvi-MacBook-Air.local>
2023-03-02 21:56:47 -0500

    seperate transformations and query generation

---
+++ generate_queries.py
@@ -0,0 +1,35 @@
+import re
+from datetime import datetime
+import dateutil.tz
+import boto3
+from botocore.exceptions import ClientError
+import json
+import pandas as pd
+
+def generate_queries(event, context):
+    
+    df_out = event[1]['df_out']
+    df_out = pd.DataFrame(df_out)
+
+    dest_table = event[0]['dest_table']
+    
+    insert = """
+    INSERT INTO public.{dest_table} (
+        """.format(dest_table=dest_table)
+    
+    columns_string = str(list(df_out.columns))[1:-1]
+    columns_string = re.sub(r' ', '\n        ', columns_string)
+    columns_string = re.sub(r'\'', '', columns_string)
+    
+    values_string = ''
+    
+    for row in df_out.itertuples(index=False,name=None):
+        row = re.sub(r'"', "'", str(row))
+        values_string += re.sub(r'nan', 'null', str(row))
+        values_string += ',\n'
+    
+    delete_statement = f"DELETE FROM {dest_table} WHERE DATETIME < NOW() - INTERVAL '7 DAY'"
+    
+    query = insert + columns_string + ')\n     VALUES\n' + values_string[:-2] + ';' + '\n' + delete_statement + ';'
+    
+    return {'query': query}
