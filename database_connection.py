5fc756c634889a7503a849acb9df41d3a504ab21
dhruvi-9 <dhruvi.antala@gmail.com>
2023-07-01 11:40:42 -0400

    remove redundant get secrets

--- database_connection.py
+++ database_connection.py
@@ -1,30 +1,10 @@
 import psycopg2
-import sys
-import os
-import boto3
-from botocore.exceptions import ClientError
-import json
+from get_secrets import get_secrets
 
 def conn(event, context):
 
     secret_name = "postgres_connection"
-    region_name = "us-east-1"
-
-    session = boto3.session.Session()
-    client = session.client(
-        service_name='secretsmanager',
-        region_name=region_name
-    )
-
-    try:
-        get_secret_value_response = client.get_secret_value(
-            SecretId=secret_name
-        )
-    except ClientError as e:
-        raise e
-
-    secret = get_secret_value_response['SecretString']
-    secret = json.loads(secret)
+    secret = get_secrets(secret_name)
 
     query_string = event['query']
     seperator = event['seperator']
@@ -44,6 +24,4 @@ def conn(event, context):
         print("Database connection failed due to {}".format(e))     
         
     conn.close()
-    cur.close()    
-    
-    return(None)
+    cur.close()
